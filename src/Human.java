b93738d563a014d53358b393de595e5900620286
nikonovdmitiy <nikonov9094@gmail.com>
2022-09-25 13:46:00 +0500

    ДЗ

--- src/Human.java
+++ src/Human.java
@@ -3,15 +3,12 @@ public class Human {
     String name;
     int dateOfBirth;
     String city;
+    String jobTitle;
 
-    Human(String name, String city, int dateOfBirth) {
-        this.name = name;
-        this.dateOfBirth = dateOfBirth;
-        this.city = city;
-    }
+
 
     void people() {
-        System.out.println("Привет!. Меня зовут "+name+". Я из города "+city+". Я родился в "+(2022-dateOfBirth)+" году. Будем знакомы!");
+        System.out.println("Привет!. Меня зовут "+name+". Я из города "+city+". Я родился в "+(2022-dateOfBirth)+" году. Я работаю на должности "+jobTitle+ ". Будем знакомы!");
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human human1 = new Human("Максим", "Минск", 35);
+        Human human1 = new Human();
+        human1.name = "Максим";
+        human1.dateOfBirth = 35;
+        human1.city = "Минск";
+        human1.jobTitle = "бренд-менеджер";
         human1.people();
 
-        Human human2 = new Human("Анна", "Москва", 29);
+        Human human2 = new Human();
+        human2.name = "Анна";
+        human2.dateOfBirth = 29;
+        human2.city = "Москва";
+        human2.jobTitle = "методист образовательных программ";
         human2.people();
 
-        Human human3 = new Human("Екатерина", "Калининград", 28);
+        Human human3 = new Human();
+        human3.name = "Екатерина";
+        human3.dateOfBirth = 28;
+        human3.city = "Калининград";
+        human3.jobTitle = "продакт-менеджер";
         human3.people();
 
-        Human human4 = new Human("Артем", "Москва", 27);
+        Human human4 = new Human();
+        human4.name = "Артем";
+        human4.dateOfBirth = 27;
+        human4.city = "Москва";
+        human4.jobTitle = "директор по развитию бизнеса";
         human4.people();
     }
 }
