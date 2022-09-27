f31ad946aeb40bb16d43bf1665460c0bac3c994f
nikonovdmitiy <nikonov9094@gmail.com>
2022-09-27 22:34:58 +0500

    ДЗ

--- src/Human.java
+++ src/Human.java
@@ -1,44 +1,29 @@
 public class Human {
 
-    String name;
-    int dateOfBirth;
-    String city;
-    String jobTitle;
+    private final String name;
+    private int dateOfBirth;
+    private final String city;
+    private final String jobTitle;
 
-    Human(String name, String city, int dateOfBirth, String jobTitle) {
+    public Human(String name, String city, int dateOfBirth, String jobTitle) {
 
-        if(name==null){
-            System.out.println("Информация не указана");
-        }else {
-        this.name = name;
-        }
+        this.name = name == null ? "Информация не указана" : name;
 
-        if(city==null){
-            System.out.println("Информация не указана");
-        }else {
-            this.city = city;
-        }
+        this.city = city == null ? "Информация не указана" : city;
 
-        if (dateOfBirth<0){
-            dateOfBirth=0;
-        }else {
+        if (dateOfBirth < 0) {
+            dateOfBirth = 0;
+        } else {
             this.dateOfBirth = dateOfBirth;
         }
 
-        if(jobTitle==null){
-            System.out.println("Информация не указана");
-        }else {
-            this.jobTitle = jobTitle;
-        }
+        this.jobTitle = jobTitle == null ? "Информация не указана" : jobTitle;
     }
-
 
 
     void people() {
-        System.out.println("Привет!. Меня зовут "+name+". Я из города "+city+". Я родился в "+(2022-dateOfBirth)+" году. Я работаю на должности "+jobTitle+ ". Будем знакомы!");
+        System.out.println("Привет!. Меня зовут " + name + ". Я из города " + city + ". Я родился в " + (2022 - dateOfBirth) + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
     }
-
-
 
 
 }
